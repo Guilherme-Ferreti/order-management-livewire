8645758bfc5685775b00313f9c9748305f416916
Guilherme Ferreti <guiferretis@gmail.com>
2023-04-30 19:29:40 -0300

    Added dashboard chart

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -37,6 +37,7 @@
         </div>
         @livewireScripts
         @stack('scripts')
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
         <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> 
         <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> 
         <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
